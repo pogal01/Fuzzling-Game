bfd7d981b6361ef156b118f388edc23cf84ea95b
THEPOGTOP\Pogal <459793@chrome.sussexdowns.ac.uk>
2018-03-05 00:16:38 +0000

    Added a start screen and fixed a few more buggs

---
+++ Fuzzling-Game-master/Assets/Options.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Options : MonoBehaviour
+{
+
+    public void Audio()
+    {
+        Debug.Log("Audio Has been selected");
+    }
+
+    public void Graphics()
+    {
+        Debug.Log("Graphics have been selected");
+    }
+}
